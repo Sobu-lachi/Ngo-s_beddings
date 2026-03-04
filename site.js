1b0edb1cf402fa589fa1267a4a2e1104e9010c82
Amaewhule Sobulachi <sobula07@gmail.com>
2026-03-04 15:32:10 +0100

    iniyial

---
+++ site.js
@@ -0,0 +1,37 @@
+const body = document.querySelector('body');
+const products = document.querySelectorAll('.product');
+const buttons = document.querySelectorAll('.selection-button');
+const form = document.querySelector('.contact-form');
+
+products.forEach(product => {
+    if (!product.classList.contains("pillow")) {
+        product.classList.add("hidden");
+    }else {
+        product.classList.remove("hidden");
+    }
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = button.dataset.target;    
+        products.forEach(product => {
+            if (product.classList.contains(target)) {
+                product.classList.remove("hidden");
+            } 
+            else {
+                product.classList.add("hidden");
+            }
+    })})});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const email = form.querySelector('input[name="email"]').value;
+    const name = form.querySelector('input[name="name"]').value;
+
+    if (!email || !name) {
+        alert("Please fill in both your name and email address.");
+        return;
+    }
+    window.location.href = `mailto:sobula07@gmail.com?subject=Contact from ${encodeURIComponent(name)}&body=Email: ${encodeURIComponent(email)}`;
+});
